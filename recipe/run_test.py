3d7e5e278cee1ea081dc6d1df9b6acd6b51c685d
Simon Billinge <simon.billinge@gmail.com>
2024-08-10 07:13:42 -0400

    cookiecuttered meta.yaml and pytest tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,2 +1,16 @@
-import diffpy.structure.tests
-assert diffpy.structure.tests.test().wasSuccessful()
+#!/usr/bin/env python
+
+import importlib
+from pathlib import Path
+import pytest
+
+# import diffpy.structure.tests
+# assert diffpy.structure.tests.test().wasSuccessful()
+
+if __name__ == "__main__":
+    package_directory = 'diffpy.structure'
+    module = importlib.import_module(package_directory)
+    module_path = Path(module.__file__).parent
+    test_location = module_path / 'tests'
+    exit_code = pytest.main([str(test_location), "-v"])
+    assert exit_code == 0
